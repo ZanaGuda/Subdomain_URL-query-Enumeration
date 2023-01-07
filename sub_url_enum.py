1aff43f1af8151960b79bdbbac1edbd18de5a99e
ZanaGuda <zanaguda28@gmail.com>
2023-01-07 12:47:43 +0100

    iterating through subdomain list to check which fit the subdomain

--- sub_url_enum.py
+++ sub_url_enum.py
@@ -9,3 +9,13 @@ print(banner)
 
 subdomain_list = open("subdomain.txt").read()
 subs = subdomain_list.readlines()
+
+for sub in subs:
+    url = f"https://{sub}.{sys.argv[1]}"
+
+    try:
+        requests.get(url)
+    except Exception as ex:
+        pass
+else:
+    print("Valid: ", url)
