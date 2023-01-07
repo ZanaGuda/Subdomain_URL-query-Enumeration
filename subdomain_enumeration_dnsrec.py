22d4be0b205dc34c5e2929c2b996a9c966890121
OrgesaQorrolli <orgesa.qorrolli@student.uni-pr.edu>
2023-01-07 23:55:22 +0200

    changes final

---
+++ subdomain_enumeration_dnsrec.py
@@ -0,0 +1,23 @@
+ import dns.resolver
+ import sys
+
+ record_types = ['A', 'AAAA', 'NS', 'CNAME', 'MX', 'PTR', 'SOA', 'TXT']
+ try:
+ domain = sys.argv[1]
+ except IndexError:
+ print('Syntax Error')
+ for records in record_types:
+ try:
+ answer = dns.resolver.resolve(domain, records)
+ print(f'\n{records} Records')
+ print('-'*30)
+ for server in answer:
+ print(server.to_text())
+ except dns.resolver.NoAnswer:
+ pass
+ except dns.resolver.NXDOMAIN:
+ (f'{domain} does not exists')
+ except KeyboardInterrupt:
+ print("Error")
+ quit()
+ 
