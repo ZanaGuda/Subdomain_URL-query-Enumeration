6b19bb2e6ccbb1934718715b71265e65b8bf8a92
ZanaGuda <zanaguda28@gmail.com>
2023-01-08 00:30:22 +0100

    changes

--- sub_url_enum.py
+++ sub_url_enum.py
@@ -1,6 +1,26 @@
-import sys
 import dns.resolver
+import sys
+import pyfiglet
 import itertools
+
+banner = pyfiglet.figlet_format("Subdomain Enumeration TOOL")
+print(banner)
+
+FILE = open('myfile.txt', 'w')
+
+
+chrs = 'bew'
+n = 6
+for i in range(1, n):
+    for j in itertools.product(chrs, repeat=i):
+        wgen = (''.join(j))
+        wordlist = wgen.splitlines()
+        FILE.writelines(wordlist)
+        FILE.write("\n")
+
+
+FILE.close()
+
 
 domain = sys.argv[1]
 
@@ -8,8 +28,9 @@ subdomain_array = ["subdomain_array.txt", "subdomains.txt"]
 
 for file in subdomain_array:
     with open(file) as inputfile:
-         content = inputfile.read()
-         subs = content.splitlines()
+        content = inputfile.read()
+        subs = content.splitlines()
+
 
 def main():
     subdomain_store = []
@@ -17,7 +38,8 @@ def main():
         try:
             ip_value = dns.resolver.resolve(f'{subdoms}.{domain}', 'A')
             if ip_value:
-                subdomain_store.append(f'{subdoms}.{domain}')
+                subdomain_store.append(
+                    f'{subdoms}.{domain}')
                 if f"{subdoms}.{domain}" in subdomain_store:
                     print(f'{subdoms}.{domain} valid')
                 else:
@@ -29,7 +51,6 @@ def main():
         except KeyboardInterrupt:
             print('Subdomain not found!')
             quit()
-
 
 
 main()
